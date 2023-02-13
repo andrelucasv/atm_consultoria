402e3a81448537f0d90dddb3e0df7180b3fc94db
Andre Lucas <alucas748@gmail.com>
2023-02-13 17:37:57 -0300

    Aplicativo finalizado

--- lib/cliente_page.dart
+++ lib/cliente_page.dart
@@ -10,6 +10,50 @@ class TelaCliente extends StatefulWidget {
 class _TelaClienteState extends State<TelaCliente> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Clientes"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset("assets/images/detalhe_cliente.png"),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Clientes",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Image.asset("assets/images/cliente1.png"),
+              ),
+              const Text(
+                "Empresa de software"
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Image.asset("assets/images/cliente2.png"),
+              ),
+              const Text(
+                "Empresa de auditoria"
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/contato_page.dart
+++ lib/contato_page.dart
@@ -10,6 +10,54 @@ class TelaContato extends StatefulWidget {
 class _TelaContatoState extends State<TelaContato> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Contato"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset("assets/images/detalhe_contato.png"),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Contato",
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  "atendimento@atmconsutoria.com.br"
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  "Telefone: (88) 3525-8888"
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  "Celular: (88) 98888-8888"
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
